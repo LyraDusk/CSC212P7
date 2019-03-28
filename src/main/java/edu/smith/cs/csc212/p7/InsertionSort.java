84a6502ca30350728192755b21c0474f0b57aefb
LyraDusk <lauren@meyerzone.net>
2019-03-28 14:55:39 -0400

    ehhhhhhh

--- src/main/java/edu/smith/cs/csc212/p7/InsertionSort.java
+++ src/main/java/edu/smith/cs/csc212/p7/InsertionSort.java
@@ -10,15 +10,19 @@ public class InsertionSort {
 		while (input.size() > 0) {
 			int value = input.removeFront();
 			Integer index = null;
-			for (int i = 0; i < input.size(); i++) {
-				if (sorted.size() == 0) {
-					sorted.addFront(value);
+			if (sorted.size() == 0) {
+				sorted.addFront(value);
+				System.out.println("a");
+				continue;
+			}
+			for (int i = 0; i < sorted.size(); i++) {
+				if (sorted.getIndex(i) > value) {
+					index = i;
+					System.out.println("b");
 					break;
 				} else {
-					if (input.getIndex(i) > value) {
-						index = i;
-						break;
-					}
+					System.out.println("c");
+					continue;
 				}
 			}
 			if (index != null) {
